fa40a499be0535340fac654b53737caef9e44f22
apple <apple@appledeMacBook-Air.local>
2022-06-27 18:08:27 +0800

    new -beta

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/new", methods=["POST"])
+def new():
+    return render_template("new.html")
